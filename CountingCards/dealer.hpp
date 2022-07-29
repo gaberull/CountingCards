b7f2447b6863dd6a0e91e64c5c8a1740c9fbf246
Gabe Scott <gabescott@gmail.com>
2022-07-29 13:03:55 -0500

    added more of the game to main.cpp

--- CountingCards/dealer.hpp
+++ CountingCards/dealer.hpp
@@ -6,6 +6,7 @@
 //
 
 #pragma once
+#include "hand.hpp"
 
 class Dealer
 {
--- CountingCards/main.cpp
+++ CountingCards/main.cpp
@@ -5,6 +5,9 @@
 //  Created by Gabe Scott on 7/25/22.
 //
 
+#include "shoe.hpp"
+#include "bank.hpp"
+#include "dealer.hpp"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -37,9 +40,28 @@ int main(int argc, const char * argv[]) {
         cin >> cutPoint;
     }
     
+    // Starting player balance
+    int playerFunds = -1;
+    cout << "Enter starting player funds from 100 to 1,000 \n";
+    cin >> playerFunds;
+    while(!cin || playerFunds < 0 || playerFunds > 3)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Wrong Input. Enter a number between 100 and 1,000 \n";
+        cin >> playerFunds;
+    }
     // Debug info
     //cout << "number of decks is " << numDecks << endl;
     //cout << "shuffle mark is  " << cutPoint << endl;
+    
+    // Create shoe and player bank, and pass them to dealer in constructor
+    Shoe shoe(numDecks, cutPoint);
+    Bank bank(playerFunds);
+    //TODO: Get number of players from user and add functionality for multiple
+    // just one player for now
+    Dealer(1);
+    
     
     return 0;
 }
