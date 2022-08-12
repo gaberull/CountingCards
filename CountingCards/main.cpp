8966c06ef965883c5106b4051912591c0edcd10a
Gabe Scott <gabescott@gmail.com>
2022-08-12 17:49:32 -0500

    edited ASCII art spacing slightly

--- CountingCards/main.cpp
+++ CountingCards/main.cpp
@@ -259,7 +259,7 @@ int main(int argc, const char * argv[])
         }
     }
     int net = bank->getBalance() - totalFunds;
-    cout << "\n\n\n\n\n";
+    cout << "\n\n\n\n";
     cout << "      _____\n";
     cout << "     |A .  | _____\n";
     cout << "     | /.\\ ||A ^  | _____\n";
